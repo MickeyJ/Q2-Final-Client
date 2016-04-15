048f0efd1a4e1c2a0b9bc7a9f19088ab4e8d2de9
Mickey <develop.agility@gmail.com>
2016-04-15 11:28:19 -0600

    testing wiki data coors

--- public/javascripts/controllers/ProductDetail.js
+++ public/javascripts/controllers/ProductDetail.js
@@ -1,5 +1,8 @@
+
 app.controller('ProductDetailController', function ($scope, $http, $window, $routeParams, CartFactory) {
   var REMOTE_API = 'https://q2-api.herokuapp.com/products/';
+  var wikiStart = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=';
+  var wikiEnd = '&format=json';
 
   loadProductDetail();
 
@@ -9,8 +12,13 @@ app.controller('ProductDetailController', function ($scope, $http, $window, $rou
   $scope.addItem = CartFactory.addItemToCart;
 
   function loadProductDetail(){
+
     $http.get(REMOTE_API + $routeParams.id)
       .then(function (data) {
+        console.log(data);
+        $http.get(wikiStart + data.data.name +wikiEnd).then(function (wiki){
+          console.log('Heloo', wiki);
+        });
         $scope.product = data.data;
       });
   }
